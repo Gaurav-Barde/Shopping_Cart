baeafdf5e3e52480684f60bb6daef4b64b4eebcc
Gaurav-Barde <gabarde25@gmail.com>
2023-07-04 14:11:59 +0530

    Fixed product name issue from the cart

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -11,7 +11,7 @@ const ProductDetails = () => {
     <Fragment>
       {items.map((product) => (
         <div key={product.id} className="my-4 border-b-2 border-gray-400">
-          <h3 className="text-2xl my-2">{getProduct(product.id)?.name}</h3>
+          <h3 className="text-2xl my-2">{getProduct(product.id)?.title}</h3>
           <h3>{product.quantity} Total</h3>
           <h3> ₹{Math.round(getProduct(product.id)?.price * 81.9)}</h3>
           <Button
