6b6fbaec9263926fd510be787a625f84c860905f
Gaurav-Barde <gabarde25@gmail.com>
2023-07-07 17:37:58 +0530

    Replaced Products ids with Stripe ids

--- src/utils/useFetch.jsx
+++ src/utils/useFetch.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { PRODUCTS_API_KEY } from "./constants";
+import { PRODUCTS_API_KEY, stripeProducts } from "./constants";
 
 const useFetch = () => {
   const [products, setProducts] = useState([]);
@@ -11,7 +11,14 @@ const useFetch = () => {
   const getProductsData = async () => {
     const data = await fetch(PRODUCTS_API_KEY);
     const json = await data.json();
-    setProducts(json.products);
+    // Replacing api ids with stripe ids
+    const stripeFormattedProducts = json?.products?.map((item) => {
+      const id = stripeProducts.find(
+        (product) => product.title === item.title
+      )?.id;
+      return { ...item, id: id };
+    });
+    setProducts(stripeFormattedProducts);
   };
 
   const getProduct = (id) => {
