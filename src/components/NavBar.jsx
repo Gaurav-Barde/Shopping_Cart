9021de4c11d3a04554a4555d273b90df4bc37b7c
Gaurav-Barde <gabarde25@gmail.com>
2023-07-13 11:45:45 +0530

    Removed Modal and added new logo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,28 +1,23 @@
-import { useState } from "react";
 import useCartContext from "../utils/useCartContex";
-import Modal from "./Modal";
+import { Link } from "react-router-dom";
+import Logo from "../assets/icons/logo.png";
 
 const NavBar = () => {
-  const [showModal, setShowModal] = useState(false);
-
   const { totalQuantity } = useCartContext();
 
-  const handleShow = () => setShowModal(true);
-  const handleClose = () => setShowModal(false);
-
   return (
-    <>
-      <div className="flex justify-between items-center">
-        <div>Ecommerce Store</div>
+    <div className="flex justify-between items-center">
+      <Link to="/">
+        <img src={Logo} className="w-12 aspect-square" alt="Logo" />
+      </Link>
+      <Link to="viewcart">
         <button
           className={`bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 my-4 border-blue-700 hover:border-blue-500 rounded`}
-          onClick={handleShow}
         >
           Cart {totalQuantity} Items
         </button>
-      </div>
-      {showModal ? <Modal handleClose={handleClose} /> : null}
-    </>
+      </Link>
+    </div>
   );
 };
 
